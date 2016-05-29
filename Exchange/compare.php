d6c27f17f62f465bbd7b47e10d0f564ac92836b2
Patryk Drozdz <patrol687@wp.pl>
2016-05-29 23:00:04 +0200

    jest tabela i wykres

---
+++ Exchange/compare.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>  
+
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Generator wykresów</title>
+        
+        <link rel="stylesheet" href="style.css" type="text/css"> 
+        
+    </head>
+    <body>
+        
+        <div class="container">
+            
+            <div class="header">
+                <a href="index.php">Strona Grówna</a>
+                
+            </div>
+             <div class="left">
+                
+                
+            </div>
+            
+            <div class="right">
+                
+                
+            </div>
+            
+            
+        </div>
+        
+    </body>
+</html>
